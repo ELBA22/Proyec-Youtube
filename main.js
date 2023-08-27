7f1bd6acf2bdb6d813983209b4e3c06d60076018
Elba Garcia <egarcias2021.net@gmail.com>
2023-08-27 00:21:53 -0500

    feat: :construction: Progreso
    Trabajando

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelectorAll(".sidebar");
 
+console.log("cambios");
+
 menuIcon.onclik =function(){
     sidebar.classList.toggle("small-sidebar");
 
